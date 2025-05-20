706cb19a0fbea29612f764b0e6703035d4da6a95
Vengatesh Dayalan <155848451+vengatesh5498@users.noreply.github.com>
2025-05-20 06:18:45 +0100

    JavaScript file created

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function playRound(humanChoice, computerChoice) {
+  // your code here!
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
